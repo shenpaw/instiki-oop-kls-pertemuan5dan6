9404dc0ab0adaa633d3fcec9be78b80e941315dc
shendy <shendy.aulia02@gmail.com>
2025-04-26 11:58:09 +0800

    Keterangan commit

--- pertemuan5602/src/pertemuan5602/Pertemuan5602.java
+++ pertemuan5602/src/pertemuan5602/Pertemuan5602.java
@@ -9,16 +9,23 @@ package pertemuan5602;
  * @author Shendy Aulia Mursyd
  * TGL: 26-04-2025
  */
+import javax.swing.JOptionPane;
 public class Pertemuan5602 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        LuasSegiTiga SegiTiga01= new LuasSegiTiga();
-        SegiTiga01.alas = 8;
-        SegiTiga01.tinggi = 5;
-        System.out.println("Luas Segitiga: "+SegiTiga01.hitungluas() );
+        // Membuat objek LuasSegiTiga
+        LuasSegiTiga SegiTiga01 = new LuasSegiTiga();
+        
+        // Meminta input alas dan tinggi menggunakan JOptionPane
+        String inputAlas = JOptionPane.showInputDialog("Masukkan alas segitiga:");
+        String inputTinggi = JOptionPane.showInputDialog("Masukkan tinggi segitiga:");
+        
+        // Mengonversi input dari String ke tipe data numerik
+        SegiTiga01.alas = (int) Double.parseDouble(inputAlas);
+        SegiTiga01.tinggi = (int) Double.parseDouble(inputTinggi);
+        
+        // Menampilkan hasil perhitungan luas segitiga
+        JOptionPane.showMessageDialog(null, "Luas Segitiga adalah " + SegiTiga01.hitungluas());
     }
-    
 }
+
